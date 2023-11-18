c23948fd93d87379c3fc80c39ed09a6f9a334125
HgThinker <nguyenhoanggia143@gmail.com>
2023-11-18 17:04:30 +0700

    Upload down_data.sh

---
+++ scripts/down_data.sh
@@ -0,0 +1,12 @@
+cd ../MUSIC-GENERATOR
+cd data
+
+#download sample data
+gdown 1-ah5TWrTlnJiHAkU7WkQuxpz8s9h7kwk
+
+#download all data
+# gdown 14M6kZS87YJx_pSLvAGIMWDGOuxwQ7FJI
+
+python -m zipfile -e music_data_sample.zip .
+# python -m zipfile -e music_data.zip .
+
