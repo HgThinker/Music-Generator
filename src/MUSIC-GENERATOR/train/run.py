dbc1f0bef5dade1266e6755653be1a7120fd63db
HgThinker <nguyenhoanggia143@gmail.com>
2023-12-04 11:17:35 +0700

    remove no_label from train() in run.py

--- src/MUSIC-GENERATOR/train/run.py
+++ src/MUSIC-GENERATOR/train/run.py
@@ -8,7 +8,6 @@ parser.add_argument('--lr', type=float, required=False, default=1e-5)
 parser.add_argument('--epochs', type=int, required=False, default=100)
 parser.add_argument('--use_wandb', type=int, required=False, default=1)
 parser.add_argument('--save_step', type=int, required=False, default=None)
-parser.add_argument('--no_label', type=int, required=False, default=0)
 parser.add_argument('--tune_text', type=int, required=False, default=0)
 parser.add_argument('--weight_decay', type=float, required=False, default=1e-5)
 parser.add_argument('--grad_acc', type=int, required=False, default=2)
@@ -23,7 +22,6 @@ train(
     epochs=args.epochs,
     use_wandb=args.use_wandb, 
     save_step=args.save_step,
-    no_label=args.no_label,
     tune_text=args.tune_text,
     weight_decay=args.weight_decay,
     grad_acc=args.grad_acc,
