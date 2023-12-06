d955b4f1bd2f540ddccc48e9defd2f95335c5870
HgThinker <102933694+HgThinker@users.noreply.github.com>
2023-12-06 22:38:04 +0700

    Update down_data.sh
    download sample data

--- scripts/down_data.sh
+++ scripts/down_data.sh
@@ -7,5 +7,5 @@ gdown 1-ah5TWrTlnJiHAkU7WkQuxpz8s9h7kwk
 #download all data
 #gdown 14M6kZS87YJx_pSLvAGIMWDGOuxwQ7FJI
 
-# python -m zipfile -e music_data_sample.zip .
-python -m zipfile -e music_data.zip .
+python -m zipfile -e music_data_sample.zip .
+# python -m zipfile -e music_data.zip .
